28ba3485332940477a9552a840d97c2f4ab5c56b
BowLuckie <bowluckie@gmail.com>
2026-02-14 23:16:42 +1100

    misc changed. fixed hard mode change debug printing style

--- console wordle.py
+++ console wordle.py
@@ -20,9 +20,9 @@ class LetterInStack(): # proper class naming conventions
 
 class WordleGame: # putting the game code in a class is better than excessive global variables
     def __init__(self):
-        self.debug_mode = False # displays internal workings for debugging
-        self.hard_mode = False 
-        self.contrast_mode = False # if its on, colour codes become bold
+        self.debug_mode:bool = False # displays internal workings for debugging
+        self.hard_mode:bool = False 
+        self.contrast_mode:bool = False # if its on, colour codes become bold
 
         if USE_JSON:
             try:
@@ -40,12 +40,12 @@ class WordleGame: # putting the game code in a class is better than excessive gl
             self.word_list = f.read().splitlines()
             self.spec_print(f'{("words available" if len(self.word_list)>1 else "words unable to be found") if self.debug_mode else ""}') # debug statement to check if the word list was loaded correctly
 
-        self.colours = self.build_colours()
+        self.colours:dict = self.build_colours()
 
-        self.keyboard = {letter: None for letter in ascii_lowercase} # creates a dict with every lettter and its colour which is None be default
+        self.keyboard:dict = {letter: None for letter in ascii_lowercase} # creates a dict with every lettter and its colour which is None be default
 
-        self.stack = []
-        self.score = 0
+        self.stack:list = []
+        self.score:int = 0
     
     def spec_print(self, text="\n", end="\n"):
         for i in text:
@@ -64,7 +64,7 @@ class WordleGame: # putting the game code in a class is better than excessive gl
         return contrast if self.contrast_mode else normal # returns the correct colour code based of contrast mode
 
     # colours can still be pretty hard to see. the nature of console based colours are also pretty limited
-    def build_colours(self): # returns the right colours based off contrast mode
+    def build_colours(self) ->  dict: # returns the right colours based off contrast mode
         return {
             "grey":   self.style("\033[90;40m", "\033[1;90;40m"),
             "yellow": self.style("\033[93;40m", "\033[1;93;40m"),
@@ -88,18 +88,20 @@ class WordleGame: # putting the game code in a class is better than excessive gl
 
 
     # displays the stack into the console with the correct colours by looping through the objects in the stack 
-    def render_stack(self):
+    def render_stack(self, render_keyboard=True):
         for row in self.stack:
             for letter in row:
                 print(self.colours[letter.colour] + letter.char.upper() + self.colours["RESET"], end="") 
             print()
-
         # make the rest of the stack grey like real wordle
         empty_rows = 6 - len(self.stack)
         for _ in range(empty_rows):
             for _ in range(5):
                 print(self.colours["grey"] + " " + self.colours["RESET"], end="")
             print()
+        # render keyboard
+        if render_keyboard:
+            self.render_keyboard()
 
     def update_keyboard(self, guess_result):
         priority = {"grey": 0, "yellow": 1, "green": 2} # colour priority. this is handy for when theres a green and a yellow/grey instance of the same letter in a word
@@ -120,9 +122,9 @@ class WordleGame: # putting the game code in a class is better than excessive gl
                 self.spec_print("wordle is a game where you have to guess a 5 letter word in 6 tries or less. after each guess, the letters will be coloured to show how close your guess was. green means the letter is in the correct position, yellow means the letter is in the word but in the wrong position and grey means the letter is not in the word at all. you can also enable hard mode which forces you to use the clues you have been given. good luck!")
             elif init_comm == "modes":
                 self.spec_print("type mode name to toggle it, type 'back' to go back")
-                self.spec_print("debug mode: " + ("enabled" if self.debug_mode else "disabled"))
-                self.spec_print("hard mode (subsequent guesses cannot use incorrect letters): " + ("enabled" if self.hard_mode else "disabled"))
-                self.spec_print("contrast mode: " + ("enabled" if self.contrast_mode else "disabled")) 
+                print("debug mode: " + ("enabled" if self.debug_mode else "disabled"))
+                print("hard mode (subsequent guesses cannot use incorrect letters): " + ("enabled" if self.hard_mode else "disabled"))
+                print("contrast mode: " + ("enabled" if self.contrast_mode else "disabled")) 
                 while True:
                     mode = input("Enter mode name: ").lower().strip()
                     if mode == "debug" or mode == "debug mode":
@@ -156,6 +158,7 @@ class WordleGame: # putting the game code in a class is better than excessive gl
     def colour_pass(self, guess, word_to_guess, wrong_letters) -> tuple[list[tuple[str, str]], list[str]]:
             checked_letters = list(word_to_guess)
             result:list = [None] * 5
+            grey_chars = []
 
             # green pass
             for i, char in enumerate(guess): # enumerate function will supply the index and the char as [(0, char), (1, char), (2, char)] allowing me to iterate over both values
@@ -172,6 +175,11 @@ class WordleGame: # putting the game code in a class is better than excessive gl
                     checked_letters[checked_letters.index(char)] = None # removes the first instance of the letter so if there are duplicate letters they arent both counted as yellow
                 else:
                     result[i] = ("grey", char)
+                    grey_chars.append(char)
+
+            # Add to wrong_letters only if the letter is not correct anywhere
+            for char in grey_chars:
+                if not any(colour in ("green", "yellow") for colour, c in result if c == char):
                     if char not in wrong_letters:
                         wrong_letters.append(char)
 
@@ -197,29 +205,51 @@ class WordleGame: # putting the game code in a class is better than excessive gl
         wrong_letters = []
         word_to_guess = choice(self.word_list)
         if self.debug_mode:
-            self.spec_print(f"DEBUG: The word to guess is '{word_to_guess}'")
+            print(f"DEBUG: The word to guess is '{word_to_guess}'")
         self.spec_print(f"\nScore: {self.colours['red'] if self.score < 0 else self.colours['green']}{self.score}{self.colours['RESET']}")
         self.render_stack() # prints an empty stack
         while True:
             guess = input("Enter your guess: ").lower().strip()
             if len(guess) != 5 or guess not in self.word_list:
-                self.spec_print(f"Invalid guess, try again. {'too short' if len(guess) != 5 else 'guess not in word list'}") # using inline if statements to compactly determine the fault
+                self.spec_print(f"Invalid guess, try again. {'wrong length' if len(guess) != 5 else 'guess not in word list'}") # using inline if statements to compactly determine the fault
                 self.render_stack()
                 continue
-            elif self.hard_mode and len(self.stack) > 0:
-                invalid_guess = False
+            elif self.hard_mode and len(self.stack) > 0: # Hard mode checks
+                # Check if all required yellow letters are in the guess
+                missing_yellows = [r for r in required_letters if r not in guess]
+                if missing_yellows: # if there are missing yellows, thow hard mode
+                    self.spec_print(f"guess must contain all previously revealed letters in wrong positions (hard mode): {', '.join(set(missing_yellows))}), try again.")
+                    self.render_stack()
+                    continue
+                # Check for wrong letters and correct positions
+                invalid_guess = False # for breakng nested loops
                 for i in guess:
                     if i in wrong_letters:
-                        self.spec_print("guess must not contain letters that have been guessed wrong (hard mode), try again.")
+                        self.spec_print("guess must not contain letters that are not in the word (hard mode), try again.")
                         self.render_stack()
                         invalid_guess = True
                         break
-                if invalid_guess: # continue off the loop 1 indent outside
+                if not invalid_guess:
+                    for i, req_char in enumerate(required_positions): # enumerate object
+                        if req_char is not None and guess[i] != req_char: # check is required character is not the same as the guess character at the same position
+                            self.spec_print("guess must have correct letters in previously correct positions (hard mode), try again.")
+                            self.render_stack()
+                            invalid_guess = True
+                            break
+                elif invalid_guess:
                     continue
 
             # check letter colours and return the result 
             result, wrong_letters = self.colour_pass(guess, word_to_guess, wrong_letters)
             self.update_keyboard(result) # update all letter colours
+            # accumulate required_letters
+            for colour, char in result:
+                if colour == "yellow" and char not in required_letters:
+                    required_letters.append(char)
+            # accumulate required_positions
+            for i, (colour, char) in enumerate(result):
+                if colour == "green":
+                    required_positions[i] = char
 
 
             # build stack
@@ -232,11 +262,11 @@ class WordleGame: # putting the game code in a class is better than excessive gl
 
             # rendering and win/loss conditions
             self.render_stack()
-            self.render_keyboard()
             if self.debug_mode:
-                self.spec_print(f"DEBUG: stack")
-                self.spec_print(f'DEBUG: {result}')
-                self.spec_print(f"DEBUG: {len(self.stack)}")
+                print(f"DEBUG: stack")
+                print(f'DEBUG: {result}')
+                print(f"DEBUG: len stack")
+                print(f"DEBUG: {len(self.stack)}")
             if guess == word_to_guess:
                 self.score += 8 - len(self.stack) # the earlier you guess the word the more points you get
                 self.spec_print(f"\nCongratulations! You've guessed the word! score: {self.score}")
